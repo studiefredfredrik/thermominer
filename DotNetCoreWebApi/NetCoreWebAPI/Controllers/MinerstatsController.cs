1dda0fac1b0215ad26bab59ff1c78f51b18fa9a7
studiefredfredrik <studiefredfredrik@gmail.com>
2018-03-11 01:00:26 +0100

    Add DotNetCoreWebApi with webpage

---
+++ DotNetCoreWebApi/NetCoreWebAPI/Controllers/MinerstatsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebAPI.Controllers
+{
+    [Route("api/minerstats")]
+    public class MinerstatsController : Controller
+    {
+        const int numberOfStatsToKeep = 100;
+        private static ConcurrentDictionary<string, List<MinerStats>> minerStats = new ConcurrentDictionary<string, List<MinerStats>>();
+
+        [HttpGet("{address}")]
+        public List<MinerStats> Get(string address)
+        {
+            minerStats.TryGetValue(address, out var resp);
+            return resp;
+        }
+
+        [HttpGet("{address}/total")]
+        public OperationStats GetTotal(string address)
+        {
+            if (string.IsNullOrEmpty(address)) return new OperationStats();
+            if(!minerStats.TryGetValue(address, out var resp))
+            {
+                return new OperationStats();
+            }
+            var rigs = resp.OrderByDescending(ppp => ppp.time).GroupBy(x => x.name).Select(grp => grp.ToList());
+
+            var rigStats = new List<RigStats>();
+            foreach (var rig in rigs)
+            {
+                rigStats.Add(new RigStats
+                {
+                    name = rig.First().name,
+                    temperatures = rig.Select(x => x.temperature.GetValueOrDefault()).ToList(),
+                    hashrates = rig.Select(x => x.hashrate.GetValueOrDefault()).ToList(),
+                    currentHashrate = rig.First().hashrate.GetValueOrDefault(),
+                    currentTemperature = rig.First().temperature.GetValueOrDefault(),
+                    currentDateTime = rig.First().time.ToUniversalTime()
+                });
+            }
+
+            return new OperationStats
+            {
+                address = address,
+                rigStats = rigStats,
+                currentHashrate = rigStats.Sum(x => x.currentHashrate),
+                currentTemperature = rigStats.First().currentTemperature,
+                lastUpdate = rigStats.First().currentDateTime.ToUniversalTime()
+            };
+        }
+
+        public class OperationStats
+        {
+            public string address;
+            public decimal currentHashrate;
+            public decimal currentTemperature;
+            public DateTime lastUpdate;
+            public List<RigStats> rigStats; 
+        }
+
+        public class RigStats
+        {
+            public string name;
+            public List<decimal> temperatures;
+            public List<decimal> hashrates;
+            public decimal currentHashrate;
+            public decimal currentTemperature;
+            public DateTime currentDateTime;
+
+        }
+
+        [HttpPost]
+        public void Post([FromBody]MinerStats value)
+        {
+            if (string.IsNullOrEmpty(value.address))
+                return;
+
+            value.time = DateTime.Now;
+
+            minerStats.AddOrUpdate(
+                value.address, 
+                new List<MinerStats>() { value },
+                (k, v) => {
+                    v.Insert(0, value);
+                    if (v.Count > numberOfStatsToKeep) v.RemoveAt(numberOfStatsToKeep);
+                    return v;
+                });
+        }
+    }
+
+    public class MinerStats
+    {
+        public string address;
+        public string name;
+        public decimal? temperature;
+        public decimal? hashrate;
+        public DateTime time;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# thermominer
+# Thermominer
+
+The purpose of Thermominer is to act as a thermostat for mining rigs, 
+allowing users to use mining rigs for heating their home instead of
+electric radiators.
+
+Thermominer will have the mining rigs start mining when the temperature is
+below a certain threshold, and stop mining once the temperature gets 
+above a certain threshold.
+
+It also features a webinterface where users can view current temperature 
+and hasrate with graphs.
+
+# Overview
+The repo consists of 3 projects:
+
+## ArduinoTemperatureSensor
+A project for reading temperature data from a TMP006 board
+
+## NetCoreWebApi
+A .net core webApi project, that also hosts the web interface
+
+## WindowsGui
+Interfaces with Arduino temperature sensor and parses ethermine output
+for hashrate info. Posts data to the WebApi
+
+
+
+
+
+
